54936b53b47ab3067d6fd8e362d472de92dd2e04
Ji-GOO <j22sooj22soo@gmail.com>
2020-08-26 13:37:10 +0900

    Part 2 Training

--- src/main/java/com/jigoo/controller/SampleController.java
+++ src/main/java/com/jigoo/controller/SampleController.java
@@ -4,12 +4,15 @@ import com.jigoo.domain.SampleDTO;
 import com.jigoo.domain.SampleDTOList;
 import com.jigoo.domain.TodoDTO;
 import lombok.extern.log4j.Log4j;
-import org.springframework.beans.propertyeditors.CustomDateEditor;
+import oracle.jdbc.proxy.annotation.Post;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.*;
 
-import java.text.SimpleDateFormat;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -90,5 +93,59 @@ public class SampleController {
         log.info("todo : " + dto);
 
         return "ex03";
+    }
+
+    @GetMapping("/ex04")
+    public String ex04(SampleDTO dto, @ModelAttribute("page") int page) {
+
+        log.info("dto : " + dto);
+        log.info("page : " + page);
+
+        return "/sample/ex04";
+    }
+
+    @GetMapping("/ex05")
+    public void ex05() {
+
+        log.info("/ex05..............");
+    }
+
+    @GetMapping("/ex06")
+    public @ResponseBody SampleDTO ex06() {
+
+        SampleDTO dto = new SampleDTO();
+        dto.setName("홍길동");
+        dto.setAge(200);
+
+        return dto;
+    }
+
+    @GetMapping("/ex07")
+    public ResponseEntity<String> ex07() {
+
+        log.info("/ex07...............");
+
+        String msg = "{\"name\": \"홍길동\"}";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json;charset=UTF-8");
+
+        return new ResponseEntity<>(msg, headers, HttpStatus.OK);
+    }
+
+    @GetMapping("/exUpload")
+    public void exUpload() {
+
+        log.info("/exUpload...........");
+    }
+
+    @PostMapping("/exUploadPost")
+    public void exUploadPost(ArrayList<MultipartFile> files) {
+
+        files.forEach(file -> {
+            log.info("------------------------------");
+            log.info("name : " + file.getOriginalFilename());
+            log.info("size : " + file.getSize());
+        });
     }
 }
---
+++ src/main/webapp/WEB-INF/views/sample/ex04.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" isELIgnored="false" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <h2>dto : ${sampleDTO }</h2>
+    <h2>page : ${page }</h2>
+</body>
+</html>
