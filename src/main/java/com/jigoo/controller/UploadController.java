28ed85ae942f24ff38a67b6d5667c5a191b49647
Ji-GOO <j22sooj22soo@gmail.com>
2020-09-11 14:02:48 +0900

    Chapter 24 of Part 6 Complete

--- src/main/java/com/jigoo/controller/UploadController.java
+++ src/main/java/com/jigoo/controller/UploadController.java
@@ -3,6 +3,8 @@ package com.jigoo.controller;
 import com.jigoo.domain.AttachFileDTO;
 import lombok.extern.log4j.Log4j;
 import net.coobird.thumbnailator.Thumbnailator;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -12,12 +14,16 @@ import org.springframework.ui.Model;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.nio.file.Files;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -40,7 +46,6 @@ public class UploadController {
     }
 
     private boolean checkImageType(File file) {
-
         try {
             String contentType = Files.probeContentType(file.toPath());
 
@@ -50,6 +55,30 @@ public class UploadController {
         }
 
         return false;
+    }
+
+    @GetMapping("/uploadForm")
+    public void uploadForm() {
+        log.info("upload form");
+    }
+
+    @PostMapping("/uploadFormAction")
+    public void uploadFormPost(MultipartFile[] uploadFile, Model model) {
+        String uploadFolder = "/Users/jjs92/Documents/upload";
+
+        for(MultipartFile multipartFile : uploadFile) {
+            log.info("------------------------------");
+            log.info("Upload File Name : " + multipartFile.getOriginalFilename());
+            log.info("Upload File Size : " + multipartFile.getSize());
+
+            File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
+
+            try {
+                multipartFile.transferTo(saveFile);
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @GetMapping("/uploadAjax")
@@ -66,17 +95,21 @@ public class UploadController {
         String uploadFolder = "/Users/jjs92/Documents/upload";
 
         String uploadFolderPath = getFolder();
-        File uploadPath = new File(uploadFolder, getFolder());
+
+        // make folder
+        File uploadPath = new File(uploadFolder, uploadFolderPath);
 
         if(uploadPath.exists() == false) {
             uploadPath.mkdirs();
         }
 
+        // make yyyy/MM/dd folder
         for(MultipartFile multipartFile : uploadFile) {
             AttachFileDTO attachDTO = new AttachFileDTO();
 
             String uploadFileName = multipartFile.getOriginalFilename();
 
+            // IE has file path
             uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);
             log.info("only file name : " + uploadFileName);
             attachDTO.setFileName(uploadFileName);
@@ -92,14 +125,18 @@ public class UploadController {
                 attachDTO.setUuid(uuid.toString());
                 attachDTO.setUploadPath(uploadFolderPath);
 
+                // check image type file
                 if(checkImageType(saveFile)) {
                     attachDTO.setImage(true);
 
                     FileOutputStream thumbnail = new FileOutputStream(new File(uploadPath, "s_" + uploadFileName));
+
                     Thumbnailator.createThumbnail(multipartFile.getInputStream(), thumbnail, 100, 100);
+
                     thumbnail.close();
                 }
 
+                // add to List
                 list.add(attachDTO);
             } catch(Exception e) {
                 e.printStackTrace();
@@ -130,5 +167,80 @@ public class UploadController {
         }
 
         return result;
+    }
+
+    @GetMapping(value = "/download", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    @ResponseBody
+    public ResponseEntity<Resource> downloadFile(@RequestHeader("User-Agent") String userAgent, String fileName) {
+        Resource resource = new FileSystemResource("/Users/jjs92/Documents/upload/" + fileName);
+
+        if(resource.exists() == false) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        String resourceName = resource.getFilename();
+
+        // remove UUID
+        String resourceOriginalName = resourceName.substring(resourceName.indexOf("_") + 1);
+
+        HttpHeaders headers = new HttpHeaders();
+
+        try {
+            String downloadName = null;
+
+            if(userAgent.contains("Trident")) {
+                log.info("IE Browser");
+
+                downloadName = URLEncoder.encode(resourceOriginalName, "UTF-8").replaceAll("\\+", " ");
+            } else if(userAgent.contains("Edge")) {
+                log.info("Edge Browser");
+
+                downloadName = URLEncoder.encode(resourceOriginalName, "UTF-8");
+
+                log.info("Edge name : " + downloadName);
+            } else {
+                log.info("Chrome Browser");
+
+                downloadName = new String(resourceOriginalName.getBytes("UTF-8"), "ISO-8859-1");
+            }
+
+            log.info("downloadName : " + downloadName);
+
+            headers.add("Content-Disposition", "attachment; filename=" + downloadName);
+        } catch(UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        return new ResponseEntity<Resource>(resource, headers, HttpStatus.OK);
+    }
+
+    @PostMapping("/deleteFile")
+    @ResponseBody
+    public ResponseEntity<String> deleteFile(String fileName, String type) {
+        log.info("deleteFile : " + fileName);
+
+        File file;
+
+        try {
+            file = new File("/Users/jjs92/Documents/upload/" + URLDecoder.decode(fileName, "UTF-8"));
+
+            file.delete();
+
+            if(type.equals("image")) {
+                String largeFileName = file.getAbsolutePath().replace("s_", "");
+
+                log.info("largeFileName : " + largeFileName);
+
+                file = new File(largeFileName);
+
+                file.delete();
+            }
+        } catch(UnsupportedEncodingException e) {
+            e.printStackTrace();
+
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<String>("deleted", HttpStatus.OK);
     }
 }
