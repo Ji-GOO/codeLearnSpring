3dd6b6714389df83a425293e56cca4ea13505faf
Ji-GOO <j22sooj22soo@gmail.com>
2020-09-11 15:38:05 +0900

    Chapter 25 of Part 6 Complete

--- src/main/java/com/jigoo/service/BoardServiceImpl.java
+++ src/main/java/com/jigoo/service/BoardServiceImpl.java
@@ -2,12 +2,14 @@ package com.jigoo.service;
 
 import com.jigoo.domain.BoardVO;
 import com.jigoo.domain.Criteria;
+import com.jigoo.mapper.BoardAttachMapper;
 import com.jigoo.mapper.BoardMapper;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -18,12 +20,25 @@ public class BoardServiceImpl implements BoardService {
     @Setter(onMethod_ = @Autowired)
     private BoardMapper mapper;
 
+    @Setter(onMethod_ = @Autowired)
+    private BoardAttachMapper attachMapper;
+
+    @Transactional
     @Override
     public void register(BoardVO board) {
 
         log.info("register.............." + board);
 
         mapper.insertSelectKey(board);
+
+        if(board.getAttachList() == null || board.getAttachList().size() <= 0) {
+            return;
+        }
+
+        board.getAttachList().forEach(attach -> {
+            attach.setBno(board.getBno());
+            attachMapper.insert(attach);
+        });
     }
 
     @Override
